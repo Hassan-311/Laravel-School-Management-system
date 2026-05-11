9e97386723380d52d0100fe30ed006f78401c7b1
Hassan-311 <hassan2406e@aptechgdn.net>
2026-05-12 00:26:35 +0500

    Fix storage permissions

--- api/index.php
+++ api/index.php
@@ -1,2 +1,8 @@
 <?php
+
+$app = require_once __DIR__ . '/../bootstrap/app.php';
+if (!file_exists(__DIR__ . '/../public/storage')) {
+    symlink(__DIR__ . '/../storage/app/public', __DIR__ . '/../public/storage');
+}
+
 require __DIR__ . '/../public/index.php';
