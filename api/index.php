59fbe1359bc9725e827487e5e964b51bb96e2044
Hassan-311 <hassan2406e@aptechgdn.net>
2026-05-13 02:59:12 +0500

    Fix api index

--- api/index.php
+++ api/index.php
@@ -1,8 +1,15 @@
 <?php
 
-$app = require_once __DIR__ . '/../bootstrap/app.php';
-if (!file_exists(__DIR__ . '/../public/storage')) {
-    symlink(__DIR__ . '/../storage/app/public', __DIR__ . '/../public/storage');
-}
+define('LARAVEL_START', microtime(true));
 
-require __DIR__ . '/../public/index.php';
+require __DIR__ . '/../vendor/autoload.php';
+
+$app = require_once __DIR__ . '/../bootstrap/app.php';
+
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+
+$response = $kernel->handle(
+    $request = Illuminate\Http\Request::capture()
+)->send();
+
+$kernel->terminate($request, $response);
