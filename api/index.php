c6595446138f87b482636710c4f9f29466269500
Hassan-311 <hassan2406e@aptechgdn.net>
2026-05-13 23:29:55 +0500

    Setup vercel deployment

--- api/index.php
+++ api/index.php
@@ -1,17 +1,3 @@
 <?php
 
-define('LARAVEL_START', microtime(true));
-
-$_ENV['APP_BASE_PATH'] = dirname(__DIR__);
-
-require dirname(__DIR__) . '/vendor/autoload.php';
-
-$app = (require_once dirname(__DIR__) . '/bootstrap/app.php');
-
-$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
-
-$response = tap($kernel->handle(
-    $request = Illuminate\Http\Request::capture()
-))->send();
-
-$kernel->terminate($request, $response);
+require __DIR__ . '/../public/index.php';
