ad36e2aec020e6707a075348a756c32429d46c37
Hassan-311 <hassan2406e@aptechgdn.net>
2026-05-14 23:10:51 +0500

    Fix SchoolClass relationship

--- app/Models/SchoolClass.php
+++ app/Models/SchoolClass.php
@@ -11,7 +11,7 @@ class SchoolClass extends Model
 
     public function students()
     {
-        return $this::hasMany(Student::class, 'class_id');
+        return $this->hasMany(Student::class, 'class_id');
     }
 
     public function subjects()
